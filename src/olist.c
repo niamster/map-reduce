f1307f3be1ffc59409de4fe7ab974739ac14a104
Dmytro Milinevskyy <milinevskyy@gmail.com>
2016-09-25 17:37:09 +0200

    olist: simple ordered list with dups

---
+++ src/olist.c
@@ -0,0 +1,157 @@
+#include <errno.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "olist.h"
+
+#if !defined(OLIST_INIT_SIZE)
+#define OLIST_INIT_SIZE 32
+#endif
+
+/* IMPL */
+
+static int ____resize(olist_storage_t *storage, unsigned elsize, unsigned count) {
+    void *elements;
+
+    elements = realloc(storage->elements, count*elsize);
+    if (!elements)
+        return -ENOMEM;
+
+    storage->elements = elements;
+    storage->size = count;
+
+    return 0;
+}
+
+static int __resize(olist_storage_t *storage, unsigned elsize) {
+    if (storage->size >= storage->count + 1)
+        return 0;
+
+    unsigned nsize = (storage->size?:1) * 2;
+
+    return ____resize(storage, elsize, nsize);
+}
+
+static int __resize_storage(olist_t *olist) {
+     return __resize(&olist->values, sizeof(olentry_t));
+}
+
+static int __resize_index(olist_t *olist) {
+    return __resize(&olist->index, sizeof(long long));
+}
+
+static int __insert(olist_t *olist, const long long pos) {
+    olentry_t *values = olist->values.values;
+    olentry_t *val = &olist->values.values[pos];
+    long long *index;
+    long long left, right;
+    long long tgt = 0;
+    int res;
+
+    if (0 == olist->index.count)
+        goto insert;
+
+    left = 0;
+    right = olist->index.count - 1;
+    index = olist->index.index;
+    while (left <= right) {
+        tgt = (left + right) / 2;
+        long long epos = index[tgt];
+        olentry_t *el = &values[epos];
+
+        int cmp = strcmp(val->key, el->key);
+        if (cmp == 0) { // dup
+            val->next = epos;
+            index[tgt] = pos;
+            return 0;
+        }
+
+        if (cmp < 0)
+            right = tgt - 1;
+        else {
+            ++tgt; // advance for insert case
+            left = tgt;
+        }
+    }
+
+  insert:
+    res = __resize_index(olist);
+    if (res != 0)
+        return res;
+    index = olist->index.index;
+    if (olist->index.count > 0)
+        memmove(index + tgt + 1, index + tgt, sizeof(olentry_t *) * (olist->index.count-tgt));
+    ++olist->index.count;
+    index[tgt] = pos;
+
+    return 0;
+}
+
+/* API */
+
+int olist_init(olist_t *olist) {
+    int res;
+
+    if (!olist)
+        return -EINVAL;
+
+    memset(olist, 0, sizeof(olist_t));
+    res = ____resize(&olist->values, sizeof(olentry_t), OLIST_INIT_SIZE);
+    if (res != 0)
+        return res;
+    res = ____resize(&olist->index, sizeof(long long), OLIST_INIT_SIZE);
+    if (res != 0)
+        goto fail;
+
+    return 0;
+
+  fail:
+    olist_destroy(olist);
+    return res;
+}
+
+int olist_insert(olist_t *olist, const char *key, void *value) {
+    olentry_t *val;
+    unsigned pos;
+    int res;
+
+    if (!olist || !key)
+        return -EINVAL;
+
+    res = __resize_storage(olist);
+    if (res != 0)
+        return res;
+
+    pos = olist->values.count++;
+
+    val = &olist->values.values[pos];
+    val->key = key;
+    val->value = value;
+    val->next = -1;
+
+    return __insert(olist, pos);
+}
+
+int olist_iterate(olist_t *olist, olist_iter_t iter, void *user) {
+    unsigned idx;
+
+    if (!olist || !iter)
+        return -EINVAL;
+
+    olist_storage_t *index = &olist->index;
+    olentry_t *values = olist->values.values;
+    for (idx=0; idx<index->count; ++idx) {
+        long long epos = index->index[idx];
+        olentry_t *el = &values[epos];
+        iter(el->key, el, olist->values.values, user);
+    }
+
+    return 0;
+}
+
+void olist_destroy(olist_t *olist) {
+    free(olist->values.elements);
+    free(olist->index.elements);
+    memset(olist, 0, sizeof(olist_t));
+}
---
+++ src/test_olist.c
@@ -0,0 +1,150 @@
+#include <CUnit/CUnit.h>
+#include <CUnit/Basic.h>
+
+#include <stdlib.h>
+#include <sys/param.h>
+#include <time.h>
+
+#include "utils.h"
+
+#include "olist.h"
+
+void test_init(void) {
+    olist_t olist;
+
+    CU_ASSERT_EQUAL_FATAL(olist_init(&olist), 0);
+    olist_destroy(&olist);
+}
+
+typedef struct {
+    unsigned uniq;
+    unsigned total;
+    const char *prev;
+} insert_iter_data_t;
+
+void __test_insert_iter(const char *key, olentry_t *entries, olentry_t *values, void *user) {
+    insert_iter_data_t *data = user;
+    olentry_t *el = entries;
+
+    CU_ASSERT_PTR_NOT_NULL_FATAL(entries);
+
+    if (data->prev)
+        CU_ASSERT_FATAL(strcmp(data->prev, key) < 0);
+
+    ++data->uniq;
+    while (true) {
+        CU_ASSERT_EQUAL_FATAL(strcmp(el->key, key), 0);
+        ++data->total;
+        if (el->next == -1)
+            break;
+        el = &values[el->next];
+    }
+    data->prev = key;
+}
+
+void __test_insert_items(olist_t *olist, const char *items[], size_t count) {
+    unsigned idx;
+
+    for (idx=0; idx<count; ++idx)
+        CU_ASSERT_EQUAL_FATAL(olist_insert(olist, items[idx], NULL), 0);
+}
+
+void test_insert(void) {
+    const char *items[] = {
+        "a", "b", "c", "d", "e", "f",
+    };
+    insert_iter_data_t udata;
+    olist_t olist;
+
+    unsigned idx; for (idx=0; idx<=ARRAY_SIZE(items); ++idx) {
+        CU_ASSERT_EQUAL_FATAL(olist_init(&olist), 0);
+
+        memset(&udata, 0, sizeof udata);
+        CU_ASSERT_EQUAL_FATAL(olist_iterate(&olist, __test_insert_iter, &udata), 0);
+        CU_ASSERT_EQUAL_FATAL(udata.uniq, 0);
+        CU_ASSERT_EQUAL_FATAL(udata.total, 0);
+        CU_ASSERT_PTR_NULL_FATAL(udata.prev);
+
+        __test_insert_items(&olist, items, idx);
+        CU_ASSERT_EQUAL_FATAL(olist.index.count, idx);
+        CU_ASSERT_EQUAL_FATAL(olist.values.count, idx);
+
+        memset(&udata, 0, sizeof udata);
+        CU_ASSERT_EQUAL_FATAL(olist_iterate(&olist, __test_insert_iter, &udata), 0);
+        CU_ASSERT_EQUAL_FATAL(udata.uniq, idx);
+        CU_ASSERT_EQUAL_FATAL(udata.total, idx);
+        if (idx > 0)
+            CU_ASSERT_EQUAL_FATAL(strcmp(udata.prev, items[idx-1]), 0);
+
+        olist_destroy(&olist);
+    }
+}
+
+void test_insert_dup(void) {
+    const char *samples[] = {
+        "a", "b", "c",
+    };
+    insert_iter_data_t udata;
+    const unsigned max = MAX(rand()%1024, ARRAY_SIZE(samples)*3);
+    const char **items = calloc(1, max * sizeof(char *));
+    unsigned idx;
+    olist_t olist;
+
+    CU_ASSERT_PTR_NOT_NULL_FATAL(items);
+
+    for (idx=0; idx<max; ++idx)
+        items[idx] = samples[rand()%ARRAY_SIZE(samples)];
+    for (idx=0; idx<ARRAY_SIZE(samples); ++idx)
+        items[rand()%max] = samples[idx];
+
+    CU_ASSERT_EQUAL_FATAL(olist_init(&olist), 0);
+
+    __test_insert_items(&olist, items, max);
+    CU_ASSERT_EQUAL_FATAL(olist.index.count, ARRAY_SIZE(samples));
+    CU_ASSERT_EQUAL_FATAL(olist.values.count, max);
+
+    memset(&udata, 0, sizeof udata);
+    CU_ASSERT_EQUAL_FATAL(olist_iterate(&olist, __test_insert_iter, &udata), 0);
+    CU_ASSERT_EQUAL_FATAL(udata.uniq, ARRAY_SIZE(samples));
+    CU_ASSERT_EQUAL_FATAL(udata.total, max);
+
+    olist_destroy(&olist);
+
+    free(items);
+}
+
+int init_suite(void) {
+    srand((unsigned)time(NULL));
+
+    return 0;
+}
+
+int clean_suite(void) {
+    return 0;
+}
+
+int main(int argc, char **argv) {
+   CU_pSuite suite = NULL;
+   (void)argc, (void)argv;
+
+   if (CUE_SUCCESS != CU_initialize_registry())
+      return CU_get_error();
+
+   if (!(suite = CU_add_suite("olist", init_suite, clean_suite)))
+       goto out;
+
+   if (!(CU_add_test(suite, "init", test_init)))
+       goto out;
+
+   if (!(CU_add_test(suite, "insert", test_insert)))
+       goto out;
+
+   if (!(CU_add_test(suite, "insert_dup", test_insert_dup)))
+       goto out;
+
+   CU_basic_run_tests();
+
+  out:
+   CU_cleanup_registry();
+   return CU_get_error();
+}
